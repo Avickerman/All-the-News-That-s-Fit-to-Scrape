bcd1885dd64fec113fa50a579c76cdb76e5502d6
Avickerman <Avickerm@gmail.com>
2018-08-10 02:22:12 -0500

    added few items

--- server.js
+++ server.js
@@ -2,26 +2,27 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
-
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 var PORT = 3000;
 
 var app = express();
 
-// Configure middleware
-// Use body-parser for handling form submissions
+
 app.use(bodyParser.urlencoded({ extended: true }));
-// Use express.static to serve the public folder as a static directory
+
 app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
+mongoose.connect("mongodb://localhost/mongoHeadlines");
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/");
+
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
   
  
